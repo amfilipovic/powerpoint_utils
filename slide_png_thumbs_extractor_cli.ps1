5c82a6daf4872ad84733ade3e4283f5c343dafa4
Antun Matija Filipović <a.m.filipovic@gmail.com>
2023-05-11 15:09:12 +0200

    master

---
+++ slide_png_thumbs_extractor_cli.ps1
@@ -0,0 +1,45 @@
+# Load the System.Windows.Forms assembly.
+Add-Type -AssemblyName System.Windows.Forms
+
+# Set the path to the current folder.
+$path = (Get-Location).Path
+
+# Create an instance of PowerPoint.
+$powerpoint = New-Object -ComObject PowerPoint.Application
+
+# Get all presentations in the current folder.
+$ppt_files = Get-ChildItem -Path $path -Filter *.pptx
+
+# Set the thumbnail size to 50% of the screen resolution.
+$screen_width = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width / 2
+$screen_height = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height / 2
+$thumbnail_size = [Math]::Round($screen_width), [Math]::Round($screen_height)
+
+# Initialize counters for the number of thumbnails and presentations processed.
+$thumbnail_count = 0
+$presentation_count = 0
+
+# Loop through each presentation and extract its slides as PNG images.
+foreach ($ppt_file in $ppt_files) {
+    # Open the presentation.
+    $presentation = $powerpoint.Presentations.Open($ppt_file.FullName)
+    # Loop through each slide in the presentation.
+    foreach ($slide in $presentation.Slides) {
+        # Generate a unique filename for the thumbnail by appending a suffix to the original filename.
+        $thumbnail_filename = $ppt_file.FullName.Replace(".pptx", "") + "_thumb_" + ($slide.SlideIndex).ToString("000") + ".png"
+        # Export the slide as a PNG image with the specified thumbnail size.
+        $slide.Export($thumbnail_filename, "png", $thumbnail_size[0], $thumbnail_size[1])
+        # Increment the thumbnail count.
+        $thumbnail_count += 1
+    }
+    # Close the presentation.
+    $presentation.Close()
+    # Increment the presentation count.
+    $presentation_count += 1
+}
+
+# Quit PowerPoint.
+$powerpoint.Quit()
+
+# Print the number of thumbnails and presentations processed.
+"Generated $thumbnail_count thumbnail(s) from $presentation_count presentation(s)."
